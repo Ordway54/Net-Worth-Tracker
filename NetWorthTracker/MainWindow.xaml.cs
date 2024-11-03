5f6e5cf623c7ad7234c1538e190b3065c469e73b
Justin Ordway <justin.ordway@aol.com>
2024-11-03 12:10:59 -0500

    Added initial Main Menu GUI and control styles

---
+++ NetWorthTracker/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace NetWorthTracker
+{
+    public partial class MainWindow : Window
+    { 
+    
+        readonly DateTime Today = DateTime.Today;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DateField.Text = Today.ToString("dddd, MMMM d, yyyy");
+        }
+    }
+}
